5e1fdee590aa8a22af5e994d30589306a58ab611
Bob(BogdanChebac) <bogdanchebac@gmail.com>
2018-03-24 12:34:55 +0200

    irina

--- public/scripts/GameEngine.js
+++ public/scripts/GameEngine.js
@@ -18,14 +18,14 @@ class GameEngine {
         };
 
         // Asset Objects
-        this.tilesImgs = {};        
+        this.tilesImgs = {};        //stocam imaginea aici
 
         // Environment Arrays
         this.tiles = [];
         this.grassTiles = [];
         this.towerEdgeTiles = [];
         this.maze = [
-            [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+            [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1], // harta
             [1, 0, 0, 1, 1, 0, 0, 0, 1, 0, 1, 1, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1],
             [1, 0, 0, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 0, 1, 0, 1, 1, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
             [1, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1],
@@ -47,7 +47,7 @@ class GameEngine {
             [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1],
             [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
         ];
-        
+
     }
 
     load() {
@@ -59,10 +59,12 @@ class GameEngine {
         const that = this;
         queue.addEventListener('complete', () => {
             that.tilesImgs.wall = queue.getResult('tile_wall');
+            that.tilesImgs.grass = queue.getResult('tile_grass')
             that.setup();
         });
         queue.loadManifest([
-            { id: 'tile_wall', src: 'img/tile_wall.png' }
+            { id: 'tile_wall', src: 'img/tile_wall.png' },
+            { id: 'tile_grass', src: 'img/tile_grass.png' }
         ]);
     }
 
@@ -88,11 +90,50 @@ class GameEngine {
     }
 
     drawTiles() {
-        const tile = new Tile('grass', { x: 0, y: 0 });
-        this.stage.addChild(tile.bmp);
-        this.tiles.push(tile);
+        // const tile = new Tile('grass', { x: 0, y: 0 });
+        // this.stage.addChild(tile.bmp);
+        // this.tiles.push(tile);
+        for(let  i = 0; i< this.tilesY; i++){
+          for(j=0; j< this.tilesX; j++){
+            if(this.maze[i][j] === 1){
+              const tile = new Tile('wall',{ x: j, y: i } )
+              this.stage.addChild(tile.bmp);
+              this.tiles.push(tile);
+            } else{
+              const tile = new Tile('grass', { x: j, y: i });
+              this.stage.addChild(tile.bmp);
+              this.tiles.push(tile);
+            }
+
+          }
+        }
     }
 }
+const towerEdgeStart = Math.round(this.tilesY/2)-2;
+
+for(let i=0; i<6; i++){
+  for(let j=0; j<4;j++){
+    if
+    (
+      i === 0 ||
+      j === 0 ||
+      j === 3 ||
+      i >= 4
+    ){
+      const tile = new Tile('wall', { x: this.tilesX - 1 +j, y:this.towerEdgeStart + i } )
+      if(j !== 0){
+        this.stage.addChild(tile.bmp)
+      }
+    }else{
+      const tile = new TIle( 'grass',{ x: this.tilesX - 1 +j, y:this.towerEdgeStart + i })
+      this.stage.addChild(tile.bmp);
+      }
+
+    }
+  };
+
+
+
 
 const gGameEngine = new GameEngine();
 export default gGameEngine;
