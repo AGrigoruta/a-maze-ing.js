069c0acf6f27694fa6a4328b549b584358294d69
Carbune Bogdan <carbunebogdan@gmail.com>
2018-04-25 17:53:50 +0300

    multiplayer

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,7 +1,13 @@
 import gGameEngine from './GameEngine.js';
-var name='dummy';
-
-
+// import io from './socket.io.js';
+var me={
+    id:null,
+    name:'dummy'
+}
+var socket=io.connect();
+var client_rooms=[];
+var curRoom;
+var roomPlayers=[];
 window.init = () => {
     gGameEngine.load();
 }
@@ -11,9 +17,9 @@ if('serviceWorker' in navigator ){
 }
 
 var setName=()=>{
-    name=document.getElementById('pnameInp').value;
+    me.name=document.getElementById('pnameInp').value;
     document.getElementById('pname').style.display='block';
-    document.getElementById('pname').innerHTML=name;
+    document.getElementById('pname').innerHTML=me.name;
     document.getElementById('pnameInp').style.display='none';
     document.getElementById('pnameBtn').innerText='Change';
     document.getElementById('pnameBtn').onclick=window.changeName;
@@ -23,11 +29,87 @@ var setName=()=>{
 window.changeName=()=>{
     document.getElementById('pname').style.display='none';
     document.getElementById('pnameInp').style.display='block';
-    document.getElementById('pnameInp').value=name;
+    document.getElementById('pnameInp').value=me.name;
     document.getElementById('pnameBtn').innerText='Set';
     document.getElementById('pnameBtn').onclick=setName;
 }
 
-window.newRoom=()=>{
-    console.log('new room')
+document.getElementById("submit").onclick=()=>{
+    let room={
+        players:document.getElementById("roomPNumber").value,
+        ai:document.getElementById("aicheck").checked,
+        connPlayers:0
+    }
+    room.name=`Room #${Math.floor((Math.random()*10000)+1)}`;
+    socket.emit('newRoom', {room:room,name:me.name});
+    curRoom=room;
+    roomPlayers.push(me);
+    document.getElementById("join").style.width=0;
+    document.getElementById("create").style.width=0;
+    document.getElementById("current").style.width='400px';
+    document.getElementById("pnameBtn").style.display='none';
 }
+
+var joinRoom=(room)=>{
+    curRoom=room;
+    socket.emit('connRoom',{room:room,name:me.name});
+    document.getElementById("join").style.width=0;
+    document.getElementById("create").style.width=0;
+    document.getElementById("current").style.width='400px';
+    document.getElementById("pnameBtn").style.display='none';
+}
+
+window.leaveRoom=()=>{
+    socket.emit('leaveRoom',curRoom);
+    document.getElementById("join").style.width='400px';
+    document.getElementById("create").style.width='400px';
+    document.getElementById("current").style.width=0;
+    document.getElementById("pnameBtn").style.display='block';
+    curRoom=null;
+}
+
+
+var addRoom=(room)=>{
+    var template = document.getElementById("roomTempl").cloneNode(true);
+    template.id=room.name;
+    template.classList.remove("template");
+    template.firstElementChild.innerHTML=room.name;
+    template.lastElementChild.onclick=()=>{
+        joinRoom(room);
+    }
+    document.getElementById("roomDispCont").appendChild(template);
+}
+
+socket.on('connect',(id)=>{
+    
+});
+
+// get rooms and my id from server
+
+socket.on('rooms',(obj)=>{
+    client_rooms=obj.rooms;
+    me.id=obj.id;
+    client_rooms.forEach(room => {
+        addRoom(room);
+    });
+});
+
+// mantain server-client data accuracy
+// start
+socket.on('newRoom',(room)=>{
+    client_rooms.push(room);
+    addRoom(room);
+});
+
+socket.on('deleteRoom',(room)=>{
+    var index=client_rooms.findIndex(x => x.name==room.name);
+    client_rooms.splice(index,1);
+    document.getElementById(room.name).remove();
+});
+
+// end
+
+socket.on('player_conn',(player)=>{
+    roomPlayers.push(player);
+    console.log(roomPlayers)
+})
--- src/server.js
+++ src/server.js
@@ -14,3 +14,62 @@ server.listen(port, null, null, () => {
 app.get('/', function (req, res) {
     res.sendFile(__dirname + '/index.html');
 });
+
+
+const io = require('socket.io')(server);
+
+this._rooms=[];
+
+io.on('connection', (socket) => {
+    socket.curRoom=null;
+    socket.name=null;
+    var leaveRoom=(room)=>{
+        socket.leave(socket.room);
+        var index=this._rooms.findIndex(x => x.name==room.name);
+        this._rooms[index].connPlayers--;
+        socket.curRoom=null;
+        if(this._rooms[index].connPlayers==0){
+            this._rooms.splice(index,1);
+            io.emit('deleteRoom',room);
+        }
+    }
+    socket.emit('rooms',{rooms:this._rooms,id:socket.id});
+    socket.on('newRoom',(obj)=>{
+        room=obj.room;
+        socket.name=obj.name;
+        socket.join(room.name);
+        socket.rooms.players=[];
+        socket.rooms.players.push({id:socket.id, name:socket.name});
+        socket.curRoom=room;
+        room.connPlayers++;
+        this._rooms.push(room);
+        io.emit('newRoom',room);
+    })
+
+    socket.on('connRoom',(obj)=>{
+        room=obj.room;
+        socket.name=obj.name;
+        if(socket.curRoom){
+            leaveRoom(socket.curRoom);
+        }
+        socket.join(room.name);
+        socket.rooms.players.push({id:socket.id, name:socket.name});
+        console.log(socket.rooms)
+        socket.curRoom=room;
+        var index=this._rooms.findIndex(x => x.name==room.name)
+        this._rooms[index].connPlayers++;
+        io.to(socket.curRoom.name).emit('player_conn',{id:socket.id,name:socket.name});
+    })
+
+    socket.on('leaveRoom',(room)=>{
+        leaveRoom(room);
+    })
+
+    socket.on('disconnect', (from) => {
+        if(socket.curRoom){
+            console.log(socket.curRoom)
+            leaveRoom(socket.curRoom);
+        }
+            
+    });
+});
